8c75297206f67d87b2efb17ac77e6aa2c46ff97f
iamdevlinph <iamdevlinph@gmail.com>
2017-10-04 18:14:15 +0800

    Style query loader

--- src/components/common/Loader.js
+++ src/components/common/Loader.js
@@ -1,16 +1,30 @@
 import React from 'react';
-import LoadingSvg from '../../styles/github.svg';
+import './Loader.scss';
 class Loading extends React.Component {
   constructor(props, context) {
     super(props, context);
-
   }
 
   render() {
-    return (<div style={{textAlign: 'center'}}><img src={LoadingSvg} alt="Loading" /></div>);
+    return (<div id="query-loader">
+      <div className="loader">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+        <defs>
+          <filter id="goo">
+            <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
+            <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9" result="goo" />
+            <feComposite in="SourceGraphic" in2="goo" operator="atop" />
+          </filter>
+        </defs>
+      </svg>
+    </div>);
   }
 }
-
-
 
 export default Loading;
