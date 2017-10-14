7d71eece7f132c455ca92e82c981726ee4f5e605
iamdevlinph <iamdevlinph@gmail.com>
2017-10-14 16:40:03 +0800

    Partial

--- src/components/Trending.js
+++ src/components/Trending.js
@@ -2,12 +2,29 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Trending = ({ trending }) => {
+  let item = trending.items;
+  let list = item.map((value, idx) => {
+    return (
+      // <li key={idx}>
+      //   <img src={value.owner.avatar_url}/>
+      //   <p>Name {value.name}</p>
+      //   <p>Full Name {value.full_name}</p>
+      //   <p>Description {value.description}</p>
+      //   <p>Visit at <a href={value.html_url}>repo</a></p>
+      //   <p>Stars {value.stargazers_count}</p>
+      //   <p>Watchers {value.watchers_count}</p>
+      //   <p>Issues {value.open_issues}</p>
+      // </li>
+      <a href="#" className="list-group-item" key={idx}>
+        <img src={value.owner.avatar_url} />
+      </a>
+    );
+  });
   return (
-    <div className="row">
-      Trending page
-      <pre>
-        {JSON.stringify(trending, null, 2)}
-      </pre>
+    <div className="row" className="trending">
+      <div className="col-lg-12 col-sm-12" className="trending--list">
+        {list}
+      </div>
     </div>
   );
 };
